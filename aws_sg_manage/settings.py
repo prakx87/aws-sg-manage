17dc9acb50567d8b5611284523e0cc017e37ee58
Prakyath Raj <prakx_rj@yahoo.co.in>
2022-03-05 22:20:37 +0530

    Update settings

--- aws_sg_manage/settings.py
+++ aws_sg_manage/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^i82ly4@2z(4be81($h9inwcvt1w*%h_7y(og(r=3khtks*@4r'
+SECRET_KEY = ''
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
